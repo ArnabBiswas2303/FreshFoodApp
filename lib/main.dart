ed697433651c328d92e33a66cb04de96185ea903
ArnabBiswas2303 <arnabdbst.99@gmail.com>
2020-03-23 09:23:29 +0530

    Refactored the folders, added new data and more
    This commit includes :
        - [x] Seperating widgets,screens and data in their own folders
        - [x] Adding meals data in dummy_data and creating meals models
        - [x] Added named routing to categories_meals_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
+import './screens/categories_meals_screen.dart';
+import './screens/categories_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,22 +14,36 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lightGreen,
         fontFamily: 'Raleway',
         accentColor: Colors.amber,
-        // canvasColor: Color.fromRGBO(255, 254, 229, 1),
-        textTheme: TextTheme(
-          body1: TextStyle(
-            color: Color.fromRGBO(20, 51, 51, 1),
-          ),
-          body2: TextStyle(
-            color: Color.fromRGBO(20, 51, 51, 1),
-          ),
-          title: TextStyle(
-            fontFamily: 'RobotoCondensed',
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+        appBarTheme: ThemeData.light().appBarTheme.copyWith(
+              textTheme: TextTheme(
+                title: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Raleway',
+                  fontSize: 24,
+                ),
+              ),
+              iconTheme: IconThemeData(
+                color: Colors.white,
+              ),
+            ),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontFamily: 'RobotoCondensed',
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
-      home: CategoriesScreen(),
+      routes: {
+        '/': (ctx) => CategoriesScreen(),
+        CategoriesMealsScreen.routeName: (ctx) => CategoriesMealsScreen(),
+      },
     );
   }
 }
